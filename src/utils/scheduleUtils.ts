0b4ac32c97fdc3d32045b02a30526c949e343597
pdavlin <patrick@davlin.io>
2022-04-13 12:52:32 -0500

    a bunch of updates

--- src/utils/scheduleUtils.ts
+++ src/utils/scheduleUtils.ts
@@ -4,17 +4,16 @@ import { format, isPast, parseISO } from "date-fns";
 import { utcToZonedTime } from "date-fns-tz";
 import { WinsAndRemainingGames } from "./types";
 
-const url = "https://api.collegefootballdata.com/games?year=2021&team=Nebraska";
+const url = "https://api.collegefootballdata.com/games?year=2022&team=Nebraska";
 
 const getCfdbKey = (): string => process.env.CFDB_KEY;
 
-const organizeAsHuskerAndOpponentScores = (schedule) => {
-  return schedule.map((game) =>
+const organizeAsHuskerAndOpponentScores = (schedule) =>
+  schedule.map((game) =>
     game.home_team === "Nebraska"
       ? { huskerScore: game.home_points, opponentScore: game.away_points }
       : { huskerScore: game.away_points, opponentScore: game.home_points }
   );
-};
 
 export const computeNumberOfWinsAndRemainingGames = (
   schedule
@@ -25,7 +24,7 @@ export const computeNumberOfWinsAndRemainingGames = (
       .map((game) => game.huskerScore && game.huskerScore > game.opponentScore)
       .reduce((acc, curr) => acc + curr),
     remainingGames: organizedSchedule
-      .map((game) => !!game.huskerScore)
+      .map((game) => (game.huskerScore ? 0 : 1))
       .reduce((acc, curr) => acc + curr),
   };
 };
@@ -40,11 +39,7 @@ export const fetchScores = async () => {
   return data;
 };
 
-export const dateInPast = (date: string) => {
-  // console.log("dateInPast");
-  return isPast(parseISO(date));
-};
-
+export const dateInPast = (date: string) => isPast(parseISO(date));
 export const formatWinLossString = (
   home_team: string,
   home_points: number,
@@ -58,20 +53,29 @@ export const formatWinLossString = (
     ? `W ${away_points}-${home_points}`
     : `L ${away_points}-${home_points}`;
 
-export const getGameTimeFromString = (game) => {
-  // console.log("getGameTimeFromString");
-  return format(
+export const getGameTimeFromString = (game) =>
+  format(
     utcToZonedTime(parseISO(game.start_date), "America/Chicago"),
     game.start_time_tbd ? "MM/dd" : "MM/dd h:mmaaaaa"
   );
+
+export const seasonStarted = (remainingGames: number): boolean =>
+  remainingGames < 12;
+
+export const getSubtitle = (warg: WinsAndRemainingGames): string => {
+  if (!seasonStarted(warg.remainingGames)) {
+    const today = new Date();
+    const kickoff = new Date(today.getFullYear(), 7, 26);
+    return `${Math.ceil(
+      (kickoff.getTime() - today.getTime()) / 86400000
+    )} days to kickoff`;
+  } else {
+    return `${warg.wins} in 2022`;
+  }
 };
 
 export const bowlIneligible = (warg: WinsAndRemainingGames): boolean =>
   6 - warg.wins > warg.remainingGames;
 
 export const getRandomNoMessage = (warg: WinsAndRemainingGames): string =>
-  bowlIneligible(warg)
-    ? "Not in 2021."
-    : ["Not yet.", "No, fam.", "Nope!", "*sigh...*", "Try next week."][
-        Math.floor(Math.random() * 5)
-      ];
+  bowlIneligible(warg) ? "Not in 2022." : "Not Yet.";
